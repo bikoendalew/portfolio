1f26ae478cef75a2e3e28f97408ff4ac27b99a64
biruk <birukendalew090@gmail.com>
2025-02-18 01:10:36 +0300

    add googleadsence

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,29 @@ import Contact from "./components/sections/Contact";
 import Hero from "./components/sections/Hero";
 import Projects from "./components/sections/Project";
 import Skills from "./components/sections/Skills";
-
+import Script from "next/script";
 
 export default function Home() {
   return (
     <div className="pt-16"> {/* Add padding-top to account for fixed navbar */}
       <Hero />
+     
+      <Script
+        strategy="afterInteractive"
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8232400557982205"
+      />
+
       <About />
+      <Script
+        strategy="afterInteractive"
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8232400557982205"
+      />
       <Projects />
       <Skills />
+      <Script
+        strategy="afterInteractive"
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8232400557982205"
+      />
       <Contact />
     </div>
   );
