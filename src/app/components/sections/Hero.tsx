0d068a64f7178a3e249cbba0458cd7302760fae4
biruk <birukendalew090@gmail.com>
2025-02-18 16:27:33 +0300

    update

--- src/app/components/sections/Hero.tsx
+++ src/app/components/sections/Hero.tsx
@@ -4,28 +4,7 @@ import Image from 'next/image';
 import { FaFacebook, FaGithub, FaLinkedin, FaTelegram } from 'react-icons/fa';
 
 export default function Hero() {
-    const socialLinks = [
-        {
-          icon: <FaGithub size={24} />,
-          url: "https://github.com/bikoendalew",
-          color: "hover:border-gray-800 hover:text-gray-800",
-        },
-        {
-          icon: <FaLinkedin size={24} />,
-          url: "https://www.linkedin.com/in/biruk-endalew-437633237/",
-          color: "hover:border-blue-600 hover:text-blue-600",
-        },
-        {
-          icon: <FaTelegram size={24} />,
-          url: "https://t.me/bikoendalew",
-          color: "hover:border-blue-500 hover:text-blue-500",
-        },
-        {
-          icon: <FaFacebook size={24} />,
-          url: "https://facebook.com/yourusername",
-          color: "hover:border-blue-700 hover:text-blue-700",
-        },
-    ];
+   
 
   return (
     <section className="min-h-screen flex items-center justify-center bg-gradient-to-b  from-gray-900 to-gray-800">
@@ -38,27 +17,7 @@ export default function Hero() {
           <p className="text-xl md:text-2xl text-gray-600 dark:text-blue-300 mb-8">
             Full Stack Developer
           </p>
-          
-          <div className="flex space-x-4 mb-8">
-            {socialLinks.map((social, index) => (
-                <a
-                key={index}
-                href={social.url}
-                target="_blank"
-                rel="noopener noreferrer"
-                className={`
-                    p-2 rounded-full
-                    border-2 border-transparent
-                    text-gray-600 dark:text-gray-400
-                    hover:scale-110
-                    transform transition-all duration-300
-                    ${social.color}
-                `}
-                >
-                {social.icon}
-                </a>
-            ))}
-            </div>
+        
 
           
           <div className="flex flex-wrap gap-4 justify-center md:justify-start">
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import Banner from "./components/Banner";
 import About from "./components/sections/About";
 import Contact from "./components/sections/Contact";
 import Hero from "./components/sections/Hero";
@@ -10,12 +9,9 @@ export default function Home() {
   return (
     <div className="pt-16"> {/* Add padding-top to account for fixed navbar */}
       <Hero />
-   
       <About />
-      <Banner dataAdSlot="3023382069" dataAdFormat="auto" dataFullWidthResponsive={false}/>
       <Projects />
       <Skills />
-      <Banner dataAdSlot="5775260807" dataAdFormat="auto" dataFullWidthResponsive={false}/>
       <Contact />
     </div>
   );
